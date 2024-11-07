6e8f4b477ac39ae1639a491b6c2c89f2bc54c038
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-08 00:34:30 +0530

    Refactor chat messaging functionality to support group messaging and timestamp generation

--- src/main/java/com/example/carpool/controller/ChatController.java
+++ src/main/java/com/example/carpool/controller/ChatController.java
@@ -2,15 +2,27 @@ package com.example.carpool.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+// import org.springframework.messaging.handler.annotation.SendTo;
 import com.example.carpool.model.ChatMessage;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 @Controller
 public class ChatController {
 
-    @MessageMapping("/message")
-    @SendTo("/topic/messages")
+    private final SimpMessagingTemplate messagingTemplate;
+
+    ChatController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    // ? Endpoint to receive messages from clients
+    @MessageMapping("/chat.sendMessage")
+    // @SendTo("/topic/messages")
     public ChatMessage sendMessage(ChatMessage message) {
+        // Broadcast the message to the group
+        // System.out.println(message.getGroupId());
+        String destination = "/topic/group." + message.getGroupId();
+        messagingTemplate.convertAndSend(destination, message);
         return message;
     }
 }
