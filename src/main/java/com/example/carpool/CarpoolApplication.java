81cfefd9b0bd955095cb2aae08c29597a01ee05b
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-08 16:05:52 +0530

    Load environment variables for database credentials using dotenv

--- src/main/java/com/example/carpool/CarpoolApplication.java
+++ src/main/java/com/example/carpool/CarpoolApplication.java
@@ -3,11 +3,17 @@ package com.example.carpool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class CarpoolApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(CarpoolApplication.class, args);
+
 	}
 
 }
