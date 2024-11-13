566f904f18cb99e0a013e2a57db5191cfcf28a36
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-13 13:56:58 +0530

    Remove dotenv usage for loading database credentials in CarpoolApplication

--- src/main/java/com/example/carpool/CarpoolApplication.java
+++ src/main/java/com/example/carpool/CarpoolApplication.java
@@ -3,15 +3,10 @@ package com.example.carpool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class CarpoolApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
 
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(CarpoolApplication.class, args);
 
 	}
